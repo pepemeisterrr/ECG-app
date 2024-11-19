684236363951109b32ebea3e8827540f3e3a43f9
pepemeister <bankovskiyk@yandex.ru>
2024-11-19 22:47:05 +0300

    default project

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    loginUser: (userData) => ipcRenderer.send('login-user', userData),
-    registerUser: (userData) => ipcRenderer.send('register-user', userData),
-    logoutUser: () => ipcRenderer.send('logout-user'),
-    receive: (channel, func) => ipcRenderer.on(channel, (event, ...args) => func(...args)),
-});
