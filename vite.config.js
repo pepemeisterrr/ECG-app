ce479b7b3e99912dabeb31bffa459b60218e3f7e
pepemeister <bankovskiyk@yandex.ru>
2024-11-26 22:24:47 +0300

    added routing

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     // Tauri expects a fixed port, fail if that port is not available
     strictPort: true,
     // if the host Tauri is expecting is set, use it
-    host: host || false,
+    host: '127.0.0.1',
     port: 5173,
   },
     // Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
